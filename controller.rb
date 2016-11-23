9df3f5f3bcc934e23fee19a5f951882fe43e0236
Wordybird <ben.a.faulkner@btinternet.com>
2016-11-23 10:30:23 +0000

    Successfully created front-end web interface.

--- controller.rb
+++ controller.rb
@@ -1,4 +1,29 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require('pry-byebug')
-require_relative('models/pizza.rb')
+require_relative('models/student.rb')
+
+get '/students' do
+  @students=Student.all()
+  erb(:index)
+end
+
+get 'students/new' do
+end
+
+post 'students' do
+end
+
+get 'students/:id' do
+end
+
+post 'students/:id' do
+end
+
+get 'students/:id/edit' do
+end
+
+post 'students/:id/delete' do
+end
+
+get 'students' do
+end
